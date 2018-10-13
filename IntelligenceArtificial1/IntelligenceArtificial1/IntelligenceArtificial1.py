d72764e28c6d04e7ccfe4e399a3d2bd09325f651
Unknown <nuno.silvapinto@outlook.com>
2018-10-13 22:25:33 +0100

    [LAB 1] Function board_perform_move

--- IntelligenceArtificial1/IntelligenceArtificial1/IntelligenceArtificial1.py
+++ IntelligenceArtificial1/IntelligenceArtificial1/IntelligenceArtificial1.py
@@ -49,25 +49,44 @@ def board_moves (board) :
             # RIGHT to LEFT
             if (c - 2 ) < 0 :
                 if is_peg(board[l][c]) and is_peg(board[l][c - 1]) and is_empty(board[l][c - 2]) :
-                    addSolutionFound(make_move(l, c), make_move(l, c - 2), listSolutionFound)
+                    addSolutionFound(make_pos(l, c), make_pos(l, c - 2), listSolutionFound)
 
             # TOP to BOTTOM
             if (l + 2 ) < M :
                 if is_peg(board[l][c]) and is_peg(board[l + 1][c]) and is_empty(board[l + 2][c]) :
-                    addSolutionFound(make_move(l, c), make_move(l + 2, c), listSolutionFound)
+                    addSolutionFound(make_pos(l, c), make_pos(l + 2, c), listSolutionFound)
 
             # BOTTOM to TOP
             if (l - 2 ) < 0 :
                 if is_peg(board[l][c]) and is_peg(board[l - 1][c]) and is_empty(board[l - 2][c]) :
-                    addSolutionFound(make_move(l, c), make_move(l - 2, c), listSolutionFound)
+                    addSolutionFound(make_pos(l, c), make_pos(l - 2, c), listSolutionFound)
 
     return listSolutionFound
 
 def addSolutionFound(initialPos, finalPos, listSolutionFound) :
-    solutionFound = []
-    solutionFound.append(initialPos)
-    solutionFound.append(finalPos)
+    solutionFound = make_move(initialPos, finalPos)
     listSolutionFound.append(solutionFound)
+
+def board_perform_move(board, move) :
+      new_board = board.copy();
+
+      l_middle_pos = abs(move[0][0] - move[1][0])
+      c_middle_pos = abs(move[0][1] - move[1][1])
+      middle_pos = make_pos(l_middle_pos, c_middle_pos)
+
+      #the ball in the original position moves
+      new_board[move[0][0]][move[0][1]] = c_empty()
+      #the ball is now in the final position
+      new_board[move[1][0]][move[1][1]] = c_peg()
+      #the other ball that was between the positions disapear
+      new_board[middle_pos[0]][middle_pos[1]] = c_empty()
+
+      return new_board
+
+
+
+
+    
 
 
 
